1c2954458ff335631864808e0dd52043e300a329
stevenjohnsoon83 <steven@eventprojection.co.uk>
2019-04-29 20:18:49 +0100

    VSC section added

--- src/App.js
+++ src/App.js
@@ -169,6 +169,21 @@ export default () => (
         <ResourceLink text="Firebase Deployment" href="https://firebase.google.com/docs/web/setup/"/>
       </Topic>
     </Section>
+    <Section id="vsc" name="Visual Studio Code">
+      <Topic name="General" parentId="vsc" index="0">
+        <ResourceLink text="Offical resources" href="https://code.visualstudio.com/docs"/>
+      </Topic>
+      <Topic name="Shortcuts" parentId="vsc" index="1">
+        <ResourceLink text="OSX" href="https://code.visualstudio.com/shortcuts/keyboard-shortcuts-macos.pdf"/>
+        <ResourceLink text="Windows" href="https://code.visualstudio.com/shortcuts/keyboard-shortcuts-windows.pdf"/>
+        <ResourceLink text="Linux" href="https://code.visualstudio.com/shortcuts/keyboard-shortcuts-linux.pdf"/>
+      </Topic>
+      <Topic name="Plugins" parentId="vsc" index="2">
+        <ResourceLink text="Debugger for Chrome" href="https://marketplace.visualstudio.com/items?itemName=msjsdiag.debugger-for-chrome"/>
+        <ResourceLink text="Browser preview" href="https://marketplace.visualstudio.com/items?itemName=auchenberg.vscode-browser-preview"/>
+      </Topic>
+    </Section>
+
     <Week1/><Week2/><Week3/><Week4/>
 
     <Week5/>
