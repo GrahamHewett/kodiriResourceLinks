e10ad3594abba6b3b293135948ed070ac55eb3a6
Graham Hewett <hewettgraham@gmail.com>
2019-10-09 11:55:34 +0100

    add deployment link

--- src/sections/Deployment.js
+++ src/sections/Deployment.js
@@ -7,9 +7,13 @@ function Deployment() {
   return (
     <Section id="deployment" name="Deployment">
       <Topic name="Deployment Options" parentId="deployment" index="0">
+      <ResourceLink
+          text="GitHub Pages React App Deployment"
+          href="https://github.com/gitname/react-gh-pages"
+        />
         <ResourceLink
           text="Now React Deployment"
-          href="https://zeit.co/guides/ deploying-react-with-now-cra"
+          href="https://zeit.co/guides/deploying-react-with-now-cra"
         />
         <ResourceLink
           text="Heroku Deployment"
